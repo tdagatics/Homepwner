5f094aa19de6e851c83867ef66ac25703054c452
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-26 11:19:31 -0700

    Added Navigation Bar

--- Homepwner/BNRAppDelegate.m
+++ Homepwner/BNRAppDelegate.m
@@ -18,9 +18,12 @@
 
     // Create a BNRItemsViewController
     BNRItemsViewController *itemsViewController = [[BNRItemsViewController alloc] init];
+    
+    // Create an instance of a UINavigationViewController, its stack only contains itemsViewController
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:itemsViewController];
 
-    // Place a BNRItemsViewController's table view in the window hierarchy
-    self.window.rootViewController = itemsViewController;
+    // Place navigation controller's view in the window hierarchy
+    self.window.rootViewController = navController;
 
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
