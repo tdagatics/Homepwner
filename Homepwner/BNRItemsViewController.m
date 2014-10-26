babdac78754ba6ded528a706af98aa654e553a2a
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-26 09:36:52 -0700

    Added Editing Mode to HeaderVie

--- Homepwner/BNRItemsViewController.m
+++ Homepwner/BNRItemsViewController.m
@@ -41,6 +41,9 @@
     [super viewDidLoad];
 
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
+    
+    UIView *header = self.headerView;
+    [self.tableView setTableHeaderView:header];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
@@ -70,6 +73,34 @@
 
 -(IBAction)toggleEditingMode:(id)sender
 {
+    // If you are currently in editing mode
+    if (self.isEditing) {
+        
+        // Change text of button to inform user of state
+        [sender setTitle:@"Edit" forState:UIControlStateNormal];
+        
+        // Turn off editing mode
+        [self setEditing:NO animated:YES];
+    } else {
+        
+        // Change text of button to inform user of state
+        [sender setTitle:@"Done" forState:UIControlStateNormal];
+        
+        // Enter editing mode
+        [self setEditing:YES animated:YES];
+    }
     
 }
+
+-(UIView *)headerView {
+    // If you have not loaded the headerView yet...
+    if (!_headerView) {
+        
+        //Load HeaderView.xib
+        [[NSBundle mainBundle] loadNibNamed:@"HeaderView" owner:self options:nil];
+    }
+    
+    return _headerView;
+}
+
 @end
